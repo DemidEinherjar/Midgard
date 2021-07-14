72c84ab234ffa9e7ad85bb685caaa4c5839c096d
Demid Rostovtsev <demid.rostovtsev@gmail.com>
2021-07-14 23:50:51 +0400

    Update parseProj

---
+++ python/parser_site/parseMain_download.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+storage_number = 0
+image_number = 1
+link = f'https://zastavok.net'
+
+for storage in range(2483):
+    responce = requests.get(f'{link}/{storage_number}/').text
+    soup = BeautifulSoup(responce, 'lxml')
+    block = soup.find('div', class_='block-photo')
+    all_image = block.find_all('div', class_='short_full')
+
+    for image in all_image:
+        image_link = image.find('a').get('href')
+        download_storage = requests.get(f'{link}{image_link}').text
+        download_soup = BeautifulSoup(download_storage, 'lxml')
+        download_block = download_soup.find('div', class_="image_data").find('div', class_="block_down")
+        result_link = download_block.find('a').get('href')
+        #Dowmload image
+        image_bytes = requests.get(f'{link}/{result_link}').content
+        with open(f'/Users/demidrostovcev/Downloads/image/{image_number}.jpg', 'wb') as file:
+            file.write(image_bytes)
+        print(f'Изображение {image_number}.jpg успешно скачено!')
+        image_number += 1
+    storage_number += 1
